fb84ca848dc9c98b0333d165233c4cad7782f0ac
Mohit-code-dot <mohit@workinxdigital.us>
2024-08-26 18:45:32 +0530

    Update text.js

--- text.js
+++ text.js
@@ -23,16 +23,12 @@ app.get("/", async (req, res) => {
   const login = req.session.login;
   const login2 = req.session.login2;
   try {
-    const uri = "mongodb+srv://WorkinX:JoPlgIK8JUpjMeuY@cluster0.qm9dld0.mongodb.net";
+    const uri = "mongodb+srv://user01:test123@cluster0.qm9dld0.mongodb.net/";
     const client = new MongoClient(uri);
     const dbName = "WorkinX"; 
     const collectionName = "brandstores";
     const collectionName2 = "images";
     await client.connect();
-    await mongoose.connect(uri, {
-      tls: true,
-      tlsCertificateKeyFile: `${__dirname}/rds-combined-ca-bundle.pem`,
-    });
     console.log("Connected to MongoDB");
 
     const db = client.db(dbName);
