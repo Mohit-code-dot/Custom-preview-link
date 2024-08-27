e1f6c6a69d4a451d6f3f8f6dac6c96770806633f
Mohit-code-dot <mohit@workinxdigital.us>
2024-08-27 17:03:04 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const { MongoClient, ObjectId } = require("mongodb");
+const session = require('express-session');
+const path = require("path");
+const app = express();
+const Port = 8000; // Use a standard port
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); // Update static file path
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  // Remove or set to false for development
+  // cookie: { secure: true }
+}));
+
+app.get("/", async (req, res) => {
+  const login = req.session.login;
+  const login2 = req.session.login2;
+  try {
+    const uri = "mongodb+srv://user01:user01@cluster0.qm9dld0.mongodb.net";
+    const client = new MongoClient(uri);
+    const dbName = "WorkinX"; 
+    const collectionName = "brandstores";
+    await client.connect();
+    console.log("Connected to MongoDB");
+    const db = client.db(dbName);
+    const collectionDB = db.collection(collectionName);
+    const idToFind = login;
+    const idToFind2 = login2;
+    console.log(idToFind);
+    console.log(idToFind2);
+    const filter = { _id: new ObjectId(idToFind) };
+    const filter2 = { _id: new ObjectId(idToFind2) };
+    const doc = await collectionDB.find(filter).toArray();
+    const doc2 = await collectionDB.find(filter2).toArray();
+
+    if (doc && doc2) {
+      res.render("test", { doc, doc2 });
+    } else {
+      res.send({ match: false }); 
+    }
+
+    await client.close();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
+app.post("/post", (req, res) => {
+  req.session.login = req.body.login;
+  req.session.login2 = req.body.login2;
+  res.redirect('/');
+});
+
+app.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
